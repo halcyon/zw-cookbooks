61d6b52201a89942814c6f22c1368cc354ed50c0
Scott McLeod <scott.mcleod@theice.com>
2011-05-19 14:15:18 -0400

    ruby it up

--- zw-rvmwrapper/recipes/default.rb
+++ zw-rvmwrapper/recipes/default.rb
@@ -7,7 +7,24 @@
 # All rights reserved - Do Not Redistribute
 #
 
-ree = "ree-1.8.7-2011.03"
+rubystring = node[:rvm][:ruby_string]
 
-execute "for i in `ls /usr/local/rvm/gems/#{ree}/bin` ; do rvm wrapper #{ree} --no-prefix $i ; done"
-execute "for i in `ls /usr/local/rvm/gems/#{ree}/bin` ; do ln -sf /usr/local/rvm/bin/$i /usr/bin/$i ; done"
+#execute "for i in `ls /usr/local/rvm/gems/#{rubystring}/bin` ; do rvm wrapper #{rubystring} --no-prefix $i ; done"
+#execute "for i in `ls /usr/local/rvm/gems/#{rubystring}/bin` ; do ln -sf /usr/local/rvm/bin/$i /usr/bin/$i ; done"
+
+ruby_block "create rvm gem wrappers" do
+  block do
+    Dir.glob("/usr/local/rvm/gems/#{rubystring}/bin/*").each do |filepath|
+      gembinary = File.basename filepath
+      if not File::exists? "/usr/local/rvm/bin/#{gembinary}"
+        system("rvm wrapper #{rubystring} --no-prefix #{gembinary}")
+      end
+      if not File::exists? "/usr/bin/#{gembinary}"
+        FileUtils.ln_s("/usr/local/rvm/bin/#{gembinary}","/usr/bin/#{gembinary}")
+      end
+    end
+  end
+  action :create
+end
+
+
