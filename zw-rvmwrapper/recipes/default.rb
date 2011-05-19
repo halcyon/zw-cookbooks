f075adf72eab2861de5b1b8a9066fcac9e53bb42
Scott McLeod <scott.mcleod@theice.com>
2011-05-19 15:02:23 -0400

    Modifications to strings, start the server for real

--- zw-rvmwrapper/recipes/default.rb
+++ zw-rvmwrapper/recipes/default.rb
@@ -7,24 +7,37 @@
 # All rights reserved - Do Not Redistribute
 #
 
-rubystring = node[:rvm][:ruby_string]
+ruby_string = node[:rvm][:ruby_string]
 
 #execute "for i in `ls /usr/local/rvm/gems/#{rubystring}/bin` ; do rvm wrapper #{rubystring} --no-prefix $i ; done"
 #execute "for i in `ls /usr/local/rvm/gems/#{rubystring}/bin` ; do ln -sf /usr/local/rvm/bin/$i /usr/bin/$i ; done"
 
 ruby_block "create rvm gem wrappers" do
   block do
-    Dir.glob("/usr/local/rvm/gems/#{rubystring}/bin/*").each do |filepath|
-      gembinary = File.basename filepath
-      if not File::exists? "/usr/local/rvm/bin/#{gembinary}"
-        system("rvm wrapper #{rubystring} --no-prefix #{gembinary}")
+    Dir.glob("/usr/local/rvm/gems/#{ruby_string}/bin/*").each do |file_path|
+      gem_binary = File.basename file_path
+      if not File::exists? "/usr/local/rvm/bin/#{gem_binary}"
+        system("rvm wrapper #{ruby_string} --no-prefix #{gem_binary}")
       end
-      if not File::exists? "/usr/bin/#{gembinary}"
-        FileUtils.ln_s("/usr/local/rvm/bin/#{gembinary}","/usr/bin/#{gembinary}")
+      if not File::exists? "/usr/bin/#{gem_binary}"
+        FileUtils.ln_s("/usr/local/rvm/bin/#{gem_binary}","/usr/bin/#{gem_binary}")
       end
     end
   end
   action :create
+end
+
+server_services = %w{ chef-solr chef-expander chef-server }
+
+if node['chef_server']['webui_enabled']
+  server_services << "chef-server-webui"
+end
+
+server_services.each do |svc|
+  service "#{svc}" do
+    supports :status => true
+    action :enable
+  end
 end
 
 
