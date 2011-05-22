84ff5aa9471b5d0aabefba55cd15bdc4308f376e
Scott McLeod <scott.mcleod@theice.com>
2011-05-22 10:56:22 -0400

    Adding zw-rhelrepos

---
+++ zw-rhelrepos/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: zw-rhelrepos
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if node['platform']['redhat']
+  yum_key "RPM-GPG-KEY-oracle-el5" do
+    url "http://public-yum.oracle.com/RPM-GPG-KEY-oracle-el5"
+    action :add
+  end
+
+  yum_repository "el5_ga_base" do
+    description "el5_ga_base"
+    name "EL5-GA-repo-base"
+    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/0/base/$basearch/"
+    key "RPM-GPG-KEY-oracle-el5"
+    action :add
+  end
+
+end
