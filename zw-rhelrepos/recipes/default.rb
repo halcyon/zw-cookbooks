2c0016f0a74d0691a2dc9f543870f840e7485a16
Scott McLeod <scott.mcleod@theice.com>
2011-05-22 11:53:08 -0400

    Remove name from repos

--- zw-rhelrepos/recipes/default.rb
+++ zw-rhelrepos/recipes/default.rb
@@ -23,7 +23,6 @@ if node['platform']['redhat']
 
   yum_repository "el5_ga_base" do
     description "el5_ga_base"
-    name "Enterprise Linux $releasever GA - $basearch - base"
     url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/0/base/$basearch/"
     key "RPM-GPG-KEY-oracle-el5"
     action :add
@@ -31,7 +30,6 @@ if node['platform']['redhat']
 
   yum_repository "el5_u5_base" do
     description "el5_u5_base"
-    name "Enterprise Linux $releasever U5 - $basearch - base"
     url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/5/base/$basearch/"
     key "RPM-GPG-KEY-oracle-el5"
     action :add
@@ -39,7 +37,6 @@ if node['platform']['redhat']
 
   yum_repository "ol5_u5_base" do
     description "ol5_u5_base"
-    name "Oracle Linux $releasever - U5 - x86_64 - base"
     url "http://public-yum.oracle.com/repo/OracleLinux/OL5/5/base/x86_64/"
     key "RPM-GPG-KEY-oracle-el5"
     action :add
@@ -47,7 +44,6 @@ if node['platform']['redhat']
 
   yum_repository "el5_addons" do
     description "el5_addons"
-    name "Enterprise Linux $releasever - $basearch - addons"
     url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/addons/$basearch/"
     key "RPM-GPG-KEY-oracle-el5"
     action :add
@@ -55,7 +51,6 @@ if node['platform']['redhat']
 
   yum_repository "el5_oracle_addons" do
     description "el5_oracle_addons"
-    name "Enterprise Linux $releasever - $basearch - oracle_addons"
     url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/oracle_addons/$basearch/"
     key "RPM-GPG-KEY-oracle-el5"
     action :add
