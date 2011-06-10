853e338bff401e2123541bc52de205021d0a922f
Scott McLeod <halcyonblue@gmail.com>
2011-06-10 10:42:44 -0400

    Yanking stuff out that doesn't belong in site-cookbooks

--- zw_rhelrepos/recipes/default.rb
+++ zw_rhelrepos/recipes/default.rb
@@ -9,59 +9,66 @@
 
 if node['platform']['redhat']
 
-  yum_key "RPM-GPG-KEY-oracle-el5" do
-    url "http://public-yum.oracle.com/RPM-GPG-KEY-oracle-el5"
+#  yum_key "RPM-GPG-KEY-oracle-el5" do
+#    url "http://public-yum.oracle.com/RPM-GPG-KEY-oracle-el5"
+#    action :add
+#  end
+
+#  yum_key "RPM-GPG-KEY-EPEL" do
+#    url "http://mirror.pnl.gov/epel/RPM-GPG-KEY-EPEL"
+#    action :add
+#  end
+
+#  yum_repository "el5_ga_base" do
+#    description "el5_ga_base"
+#    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/0/base/$basearch/"
+#    key "RPM-GPG-KEY-oracle-el5"
+#    action :add
+#  end
+
+#  yum_repository "el5_u5_base" do
+#    description "el5_u5_base"
+#    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/5/base/$basearch/"
+#    key "RPM-GPG-KEY-oracle-el5"
+#    action :add
+#  end
+
+#  yum_repository "ol5_u5_base" do
+#    description "ol5_u5_base"
+#    url "http://public-yum.oracle.com/repo/OracleLinux/OL5/5/base/x86_64/"
+#    key "RPM-GPG-KEY-oracle-el5"
+#    action :add
+#  end
+
+#  yum_repository "el5_addons" do
+#    description "el5_addons"
+#    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/addons/$basearch/"
+#    key "RPM-GPG-KEY-oracle-el5"
+#    action :add
+#  end
+
+# yum_repository "el5_oracle_addons" do
+#    description "el5_oracle_addons"
+#    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/oracle_addons/$basearch/"
+#    key "RPM-GPG-KEY-oracle-el5"
+#    action :add
+#  end
+
+#  yum_repository "epel" do
+#    description "epel"
+#    url "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=$basearch"
+#    mirrorlist true
+#    key "RPM-GPG-KEY-EPEL"
+#    action :add
+#  end
+
+#  execute "yum update -y"
+
+  yum_repository "epel-erlang"
+    description "epel-erlang"
+    url "baseurl=http://repos.fedorapeople.org/repos/peter/erlang/epel-$releasever/$basearch/"
     action :add
   end
 
-  yum_key "RPM-GPG-KEY-EPEL" do
-    url "http://mirror.pnl.gov/epel/RPM-GPG-KEY-EPEL"
-    action :add
-  end
-
-  yum_repository "el5_ga_base" do
-    description "el5_ga_base"
-    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/0/base/$basearch/"
-    key "RPM-GPG-KEY-oracle-el5"
-    action :add
-  end
-
-  yum_repository "el5_u5_base" do
-    description "el5_u5_base"
-    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/5/base/$basearch/"
-    key "RPM-GPG-KEY-oracle-el5"
-    action :add
-  end
-
-  yum_repository "ol5_u5_base" do
-    description "ol5_u5_base"
-    url "http://public-yum.oracle.com/repo/OracleLinux/OL5/5/base/x86_64/"
-    key "RPM-GPG-KEY-oracle-el5"
-    action :add
-  end
-
-  yum_repository "el5_addons" do
-    description "el5_addons"
-    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/addons/$basearch/"
-    key "RPM-GPG-KEY-oracle-el5"
-    action :add
-  end
-
-  yum_repository "el5_oracle_addons" do
-    description "el5_oracle_addons"
-    url "http://public-yum.oracle.com/repo/EnterpriseLinux/EL5/oracle_addons/$basearch/"
-    key "RPM-GPG-KEY-oracle-el5"
-    action :add
-  end
-
-  yum_repository "epel" do
-    description "epel"
-    url "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=$basearch"
-    mirrorlist true
-    key "RPM-GPG-KEY-EPEL"
-    action :add
-  end
-
-  execute "yum update -y"
 
 end
