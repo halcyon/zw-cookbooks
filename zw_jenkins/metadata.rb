da51ea885d9d132e492721974f47f9417c83f117
Scott McLeod <halcyonblue@gmail.com>
2011-06-16 13:33:34 -0400

    Updating metadata.rb to pull in correct cookbooks

--- zw_jenkins/metadata.rb
+++ zw_jenkins/metadata.rb
@@ -4,3 +4,11 @@ license          "All rights reserved"
 description      "Installs/Configures zw_jenkins"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+%w{ java zw_jks }.each do |cb|
+    depends cb
+end
+
+%w{ debian ubuntu centos redhat fedora }.each do |os|
+    supports os
+end
