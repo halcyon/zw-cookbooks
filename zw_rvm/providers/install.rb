96be7157507c0391f4a339b3b42cc6dc617fa449
Scott McLeod <halcyonblue@gmail.com>
2011-06-17 15:57:14 -0400

    Worked out algorith for install provider

--- zw_rvm/providers/install.rb
+++ zw_rvm/providers/install.rb
@@ -1,7 +1,23 @@
 action :create do
   ruby_block "install ruby" do
     block do
+      requrie 'rubygems'
       require 'rvm'
+
+      current=RVM::Environment.current
+
+      rvm_strings = current.list_strings()
+      puts "rvm_strings "
+      p rvm_strings
+
+      puts "to be installed: "
+      p requested.select { |request|
+        rvm_strings.select
+        { |rvm|
+          ! rvm.scan(request).empty?
+        }.empty?
+      }
+
     end
     action :create
   end
